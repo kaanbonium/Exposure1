0c9cc59016735e80cc11c23c9faa555f3662189e
Josiah <josiah.lugo@vtsd.com>
2025-05-30 11:11:40 -0400

    Finished Project

--- Introduction/Lab03.java
+++ Introduction/Lab03.java
@@ -4,12 +4,12 @@ public class Lab03 {
     public static void main(String[] args) {
         System.out.println("Lab03, 100 Point Version\n");
 
-         int milliseconds = 10000123;
-         int hours = milliseconds/3600000;
-         int remainder = 10000 % 3600;
-         int minutes = remainder/60;
-         int seconds = remainder % 60;
-         int miliseconds = milliseconds % 1000;
+        int milliseconds = 10000123;
+        int hours = milliseconds / 3600000;
+        int remainder = milliseconds % 3600000;
+        int minutes = remainder / 60000;
+        int seconds = (remainder % 60000) / 1000;
+        int miliseconds = milliseconds % 1000;
 
         System.out.println("Starting milli-seconds: \t" + milliseconds);
         System.out.println("Hours: \t\t\t\t\t\t" + hours);
